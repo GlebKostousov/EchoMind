534fcaab5eac6d112783ef5261635dd67c4dfc50
GlebKostousov <kostousjr@gmail.com>
2025-10-20 18:51:15 +0300

    Создал систему конфигурации и констант

---
+++ src/tools/const.py
@@ -0,0 +1,56 @@
+"""Модуль для хранения констант и конфигураций"""
+
+from pathlib import Path
+
+from sentence_transformers import SentenceTransformer
+
+__all = ("app_settings",)
+from pydantic import BaseModel
+from pydantic_settings import BaseSettings
+
+__all__ = ("app_settings",)
+
+
+class Frida(BaseModel):
+    """Модель для эмбеддинга"""
+
+    model: SentenceTransformer = SentenceTransformer("ai-forever/FRIDA")
+    frida_input_token: int = 512
+    frida_output_token: int = 1536
+
+
+class Qdrant(BaseModel):
+    """Конфиг настройки Qdrant"""
+
+    http_port: int = 6333
+    location: str = "localhost"
+    collection_name: str = "sentence_collection"
+
+
+class ChunkerModel(BaseModel):
+    """Настройка модели чанкирования"""
+
+    similarity_threshold: float = 0.8
+    chunk_models: str = "minishlab/potion-base-8M"
+    chunk_models_lang: str = "ru"
+    min_sentences_per_chunk: int = 2
+
+
+class AllPath(BaseModel):
+    """Все пути"""
+
+    src_url: Path = Path(__file__).parent.parent
+    chunk_models_local_dir: Path = src_url / "vector_way" / "chunk_model"
+    embedding_model_local_dir: Path = src_url / "vector_way" / "vector_way"
+
+
+class Settings(BaseSettings):
+    """Общие настройки приложения"""
+
+    path_config: AllPath = AllPath()
+    chunker_config: ChunkerModel = ChunkerModel()
+    embedding_model_config: Frida = Frida()
+    qdrant_config: Qdrant = Qdrant()
+
+
+app_settings = Settings()
