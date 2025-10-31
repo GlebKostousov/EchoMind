dd2e44d57dc6c027e2441d1994bc7be67cf78850
GlebKostousov <kostousjr@gmail.com>
2025-10-31 15:45:19 +0300

    refactor(base-vector): Добавил возможность квантовать модель

--- src/core/embeddings_models/dense_vector/base_vector_model.py
+++ src/core/embeddings_models/dense_vector/base_vector_model.py
@@ -14,35 +14,35 @@ from core.embeddings_models.model_config import BaseVectorModel
 logger = setup_logger(__name__)
 
 
-# TODO: проверь версию модели и онлайн, обнови если надо
-# TODO: добавь атрибут о запуске многозадачности. Если он True,
-#  то запускаем с декоратором @pool, который
-#  перед запуском ecode `pool = model.start_multi_process_pool(target_devices=["cuda:0", "cuda:1"])`
-#  логика encode и после логики `model.stop_multi_process_pool(pool)`
-
-
 class EchoMindEmbedding:
     """Базовый класс для создания любой модели эмбеддингов"""
 
-    def __init__(self, validate_data: BaseVectorModel) -> None:
+    def __init__(
+        self,
+        validate_data: BaseVectorModel,
+        quant: bool,
+        type_of_quant_backend: Literal["torch", "onnx", "openvino"] = "openvino",
+    ) -> None:
         """
         Инициализация данных для создания модели
 
-        [x] 1.  Принимаем имя с HF и путь до папки.
-        [x] 1.1 Проверяем приватность.
-        [x] 1.2 После успешной проверки приватности:
-        [x]     1.3. Если модель приватная и есть валидный токен, то переходим в 1.5.
-        [x]     1.4.    Если нет, то raises
-        [x] 1.5.    Валидируем полученные значения
-        [x] 2.  Проверяем есть ли эта модель в этой папке
-        [x] 3.0  Если есть, то:
-        [x]     3.1  Создаем экземпляр ST, вместо названия - путь до модели
-        [x] 3.2 Если нет, то:
-        [x]     3.3 Скачиваем в папку по адресу через `snapshot_download`.
-        [x]     3.4 Выполняем пункт `3.1`
+        1.  Принимаем имя с HF и путь до папки.
+        1.1 Проверяем приватность.
+        1.2 После успешной проверки приватности:
+            1.3. Если модель приватная и есть валидный токен, то переходим в 1.5.
+            1.4.    Если нет, то raises
+        1.5.    Валидируем полученные значения
+        2.  Проверяем есть ли эта модель в этой папке
+        3.0  Если есть, то:
+            3.1  Создаем экземпляр ST, вместо названия - путь до модели
+        3.2 Если нет, то:
+            3.3 Скачиваем в папку по адресу через `snapshot_download`.
+            3.4 Выполняем пункт `3.1`
 
         Args:
             validate_data (BaseVectorModel): провалидированные данные, необходимые для создания
+            quant (bool): указание о необходимости квантования модели
+            type_of_quant_backend(str): встроенный в SentenceTransformer способ квантования
 
         """
         self._validate_data: BaseVectorModel = validate_data
@@ -52,6 +52,8 @@ class EchoMindEmbedding:
         self.is_model_downloaded: bool = False
 
         self._model: SentenceTransformer | None = self._create_model()
+        self.quant = quant
+        self.type_of_quant_backend = type_of_quant_backend
 
     def _create_model(self) -> SentenceTransformer | None:
         try:
@@ -60,9 +62,6 @@ class EchoMindEmbedding:
                 self.download_model_for_local_path()
                 self.is_model_downloaded = True
 
-            # TODO: Проверь, как будут работать приватные модели
-            # TODO: Если надо квантование, то передать при создании
-            #  SentenceTransformer model_kwargs={"torch_dtype": "bfloat16"}
             return SentenceTransformer(
                 model_name_or_path=self._validate_data.local_path_for_downloads,
                 local_files_only=True,
@@ -126,9 +125,7 @@ class EchoMindEmbedding:
             bool: Если True, значит модель уже есть. Если False, то модель отсутствует.
 
         """
-        local_path = self._validate_data.local_path_for_downloads
-        model_name = self._validate_data.models_name_from_hf
-
+        local_path: Path = self._generate_local_path()
         model_dir = Path(local_path)
         required_files = [
             model_dir / MODULES_JSON_TO_EXISTS_CHECK,
@@ -136,10 +133,25 @@ class EchoMindEmbedding:
         ]
         result = all(file.exists() for file in required_files)
         if not result:
-            msg = f"Отсутствует скачанная модель {model_name} по пути: {local_path}"
+            msg = f"Отсутствует скачанная модель {self._validate_data.models_name_from_hf} по пути: {local_path}"
             logger.warning(msg)
 
         return result
+
+    def _generate_local_path(self) -> Path:
+        """
+        Функция помощник, которая генерирует правильное название структуры хранения моделей.
+        Структура: папка заданная пользователем (или папка по-умолчанию) -> название модели ->
+        спец папка для квантованных моделей
+
+        Returns:
+            Path: путь конечной папки выбранной модели
+
+        """
+        # папка / название модели / quant (если да)
+        model_name = self._validate_data.models_name_from_hf
+        path = self._validate_data.local_path_for_downloads / model_name
+        return (path / "quant") if self.quant else path
 
     def download_model_for_local_path(self) -> None:
         """
@@ -147,15 +159,14 @@ class EchoMindEmbedding:
 
         token (Optional[str]): токен для доступа к приватной модели HF
         model_name_from_hf (str): название модели с сайта HF
-        path_to_download_model (str|Path): Путь или строка пути до папки,
-        где будет храниться модель
+        path_to_download_model (str|Path): Путь или строка пути до папки с местом хранения моделей
 
         Returns:
             None
 
         """
         model_name_from_hf = self._validate_data.models_name_from_hf
-        path_to_download_model = self._validate_data.local_path_for_downloads
+        path_to_download_model = self._generate_local_path()
 
         if not self._check_existing_model_repo():
             msg = (
@@ -172,33 +183,31 @@ class EchoMindEmbedding:
         else:
             msg = f"Начинаю загрузку модели {model_name_from_hf}"
             logger.info(msg)
-            local_path = Path(path_to_download_model)
+
+            local_path = path_to_download_model
+            parent_path = local_path.parent if self.quant else local_path
 
             snapshot_download(
                 repo_id=model_name_from_hf,
-                local_dir=local_path,
-                cache_dir=local_path,
+                local_dir=parent_path,
+                cache_dir=parent_path,
             )
-            msg = f"Модель {model_name_from_hf} загружена по пути: {local_path}"
+            if self.quant:
+                quant_model = SentenceTransformer(
+                    model_name_or_path=str(parent_path),
+                    backend=self.type_of_quant_backend,
+                )
+                msg = f"Модель {model_name_from_hf} квантована"
+                logger.info(msg)
+                quant_model.save_pretrained(str(local_path))
+
+            msg = (
+                f"Модель {model_name_from_hf} загружена по пути: {local_path if self.quant else parent_path}.\n"
+                f"Было применено квантование -> {'Да' if self.quant else 'Нет'}."
+            )
             logger.info(msg)
 
 
 if __name__ == "__main__":
     frida_folder: str = "frida"
-    hf_models_path = Path(
-        r"D:\PythonProject\EchoMind\src\core\embeddings_models\local_file_embedding_models",
-    )
-    hf_models_path_openvino = Path(
-        r"D:\PythonProject\EchoMind\src\core\embeddings_models\local_file_embedding_models\openvino",
-    )
     model_name_frida = "ai-forever/FRIDA"
-    type_of_backend_hf: Literal["torch", "onnx", "openvino"] = "openvino"
-
-    test_file_path: str = (
-        r"D:\PythonProject\EchoMind\src\storage\data_from_local_files\test.txt"
-    )
-
-    # download_model_for_local_path(
-    #     model_name_from_hf=model_name_frida,
-    #     path_to_download_model=Path(hf_models_path / frida_folder),
-    # )
